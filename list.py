fa6d47be694dba8387fde5a695cd8d3a24a2a44a
Tom <36845000+prawdziwytomasz@users.noreply.github.com>
2018-03-01 20:58:36 +0100

    lesson 1- list

---
+++ list.py
@@ -0,0 +1,13 @@
+
+
+
+a=[1,1,2,3,4,5,6,7,8,9]
+print(a) #list
+print(a[len(a)-1]) # the last elements
+print(a[len(a)-2]) #penultimate element
+print(max(a)) # kth element
+print (len(a)) #number of elements
+a.reverse()
+print(a) #revesed list
+
+
